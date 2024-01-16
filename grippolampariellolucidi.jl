0d9a6e7d0731643aaa66d4917d0adce4151e15f3
Samara Viriato Vilar Dias <106534452+SamaraVilar@users.noreply.github.com>
2024-01-16 20:44:27 -0300

    Add files via upload

---
+++ grippolampariellolucidi.jl
@@ -0,0 +1,36 @@
+#
+#   Linesearch nonmonotone Grippo-Lampariello-Lucidi
+#
+
+function gll(x, f, gradfx, projf, eta, fx, sigma1, sigma2)
+    t0 = time()
+
+    alpha = 1.0  # Inicialize α aqui
+
+    while true
+        dk = projf(x - alpha * gradfx) - x # ATENÇÃO
+        GD = eta * dot(dk, gradfx)
+        q = projf(x - alpha * gradfx)
+        fq = f(q)
+        f_max = f(x) # Não consegui uma forma de calcular a f_max como está no algoritmo
+        
+        if fq ≤ f_max + GD
+            et = time() - t0
+            return (alpha, et)
+            break
+        end
+        
+        # Quadratic interpolation
+        if alpha ≤ 0.1
+            alpha = alpha / 2.0
+            et = time() - t0
+            return (alpha, et)
+        else
+            at = -alpha^2 * dot(gradfx, dk) / (2 * (fq - fx - alpha * dot(gradfx, dk)))
+            at < sigma1 || at > sigma2 * alpha && (at = at / 2.0)
+            alpha = at
+            et = time() - t0
+            return (alpha, et)
+        end
+    end
+end
---
+++ maincutestspg.jl
@@ -0,0 +1,42 @@
+#
+# Test with Price-Dixon function
+#
+
+include("spectralprojectedgradient.jl");
+include("grippolampariellolucidi.jl");
+
+using LinearAlgebra, DataFrames, Random, Printf, Plots, CUTEst, NLPModels
+
+nlp=CUTEstModel("PRICE3")
+
+x0 = [1.0; 2.0]
+
+function f(x)
+    return obj(nlp,x)
+end
+
+function gradf(x)
+    return grad(nlp,x)
+end
+
+function ortogonal(n)
+    a = Float64[]
+    push!(a, -1/sqrt(2))
+    push!(a, 1)
+    for i in 3:n
+        push!(a, 0)
+    end 
+    return a
+end
+
+function projf(x)
+    n=length(x)
+    a=ortogonal(n)
+    b=0
+    px=x+(b-dot(a,x))*a/norm(a)^2
+    return px
+end
+
+(x,ierror,info,etime,seqx) = spg(x0, f, gradf, projf, 1.e-30, 1.e+30, 1.e-6, 1.e-4, 1, 100, gll)
+
+finalize(nlp)
